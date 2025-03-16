f5ae6c003749c7533664dcf4c1a3535bacbe339d
Yupi <youngperchik2404@gmail.com>
2025-03-16 20:38:39 +0500

    Created a Money class

---
+++ Money.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyClasses
+{
+    public class Money
+    {
+        public double Sum { get; set; }
+
+
+
+        public static implicit operator Money(double sum)
+        {
+            return new Money(sum);
+        }
+
+
+        public static Money operator+ (Money num1, Money num2)
+        {
+            Money res = num1 + num2;
+            return res;
+        }
+        public static Money operator +(Money num1, double num2)
+        {
+            Money res = num1 + num2;
+            return res;
+        }
+
+        public static Money operator- (Money num1, Money num2)
+        {
+            Money res = num1 - num2;
+            return res;
+        }
+
+        public static Money operator- (Money num1, double num2)
+        {
+            Money res = num1 - num2;
+            return res;
+        }
+
+
+
+        public static Money operator* (Money num1, int num2)
+        {
+            Money res = num1 * num2;
+            return res;
+        }
+
+        public static Money operator/ (Money num1, int num2)
+        {
+            Money res = num1 / num2;
+            return res;
+        }
+
+
+
+        public static Money operator++ (Money sum)
+        {
+            Money res = sum;
+            res += 0.1;
+            return res;
+        }
+
+        public static Money operator-- (Money sum)
+        {
+            Money res = sum;
+            res -= 0.1;
+            return res;
+        }
+
+
+
+        public static bool operator> (Money num1, Money num2)
+        {
+            return num1 > num2;
+        }
+
+        public static bool operator <(Money num1, Money num2)
+        {
+            return num1 < num2;
+        }
+
+        public static bool operator ==(Money num1, Money num2)
+        {
+            return num1 == num2;
+        }
+
+        public static bool operator !=(Money num1, Money num2)
+        {
+            return num1 != num2;
+        }
+
+        public Money(double sum)
+        {
+            Sum = sum;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿namespace MoneyClasses
+{
+    public class MyException : ApplicationException
+    {
+        public DateTime TimeException { get; private set; }
+        public MyException() : base("Мое исключение")
+        {
+            TimeException = DateTime.Now;
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Money num1 = 100;
+            Money num2 = 200;
+
+
+            try
+            {
+                Console.WriteLine(num1 + num2);
+                Console.WriteLine(num1 - num2);
+                Console.WriteLine(num1 * 2);
+                Console.WriteLine(num1 / 2);
+                Console.WriteLine(num1 == num2);
+                Console.WriteLine(num1 != num2);
+                Console.WriteLine(num1 > num2);
+                Console.WriteLine(num1 < num2);
+                Console.WriteLine(num1++);
+                Console.WriteLine(num2++);
+                Console.WriteLine(num1--);
+                Console.WriteLine(num2--);
+            }
+            catch (Exception ex)
+            {
+                throw new MyException();
+            }
+        }
+    }
+}
